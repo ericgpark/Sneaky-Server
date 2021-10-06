726b36d418398bc6cdb5ed7eca6d57081f241c6a
Eric Park <ericparkmusic@gmail.com>
2021-10-06 15:34:01 -0700

    Set up api endpoint to get_shoes function

--- sneakyserver/urls.py
+++ sneakyserver/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from sneaky_api.views import get_shoes
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', get_shoes),
 ]
